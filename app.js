9749d376f99f77813c1887dc36ecf66ec59d27bd
Lailah Berry <lailahnicole614@gmail.com>
2023-01-10 16:36:19 -0800

    this allows functions to be called

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-/* Imports */
-
+/* Imports and Get Dom Elements */
+const kanyeButtonEl = document.getElementById('kanye-button');
+const peteButtonEl = document.getElementById('pete-button');
+const doctorButtonEl = document.getElementById('doctor-button');
 /* Get DOM Elements */
 
+const kanyeContainer = document.getElementById('kanye-container');
+const peteContainer = document.getElementById('pete-container');
+const doctorContainer = document.getElementById('doctor-container');
+
+const correctGuessesEL = document.getElementById('wins');
+const incorrectGuessesEl = document.getElementById('losses');
+const totalGuessesEl = document.getElementById('total');
+
 /* State */
+let correctGuesses = 0;
+let totalGuesses = 0;
 
 /* Events */
+kanyeButtonEl.addEventListener('click', () => {
+    const correctSpot = getRandomHidingSpot();
+    handleGuess('kanye', correctSpot);
+});
 
 /* Display Functions */
 
